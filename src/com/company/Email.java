f75f5673bf4bb7cd9554fcbe409a53b2f0045a92
Lucefull <luansteffens@gmail.com>
2020-06-05 22:34:08 -0300

    Email
    Atributos classe email

--- src/com/company/Email.java
+++ src/com/company/Email.java
@@ -1,4 +1,24 @@
 package com.company;
 
 public class Email {
+    String[] remetente = new String[10];
+
+    public Email(String[] remetente) {
+        this.remetente = remetente;
+    }
+
+    public Email(String destinatario,String assunto,String corpo){
+
+    }
+    public Email(String remetente,String destinatario,String assunto,String corpo){
+
+    }
+
+    public String[] getRemetente() {
+        return remetente;
+    }
+
+    public void setRemetente(String[] remetente) {
+        this.remetente = remetente;
+    }
 }
